089b70da085b618831fd5ac2aab08300a603bd72
dovakeen118 <kegillis118@gmail.com>
2022-09-26 11:09:05 -0400

    finished fac code

--- server.rb
+++ server.rb
@@ -5,22 +5,21 @@ require "sinatra/reloader" if development?
 set :bind, '0.0.0.0'  # bind to all interfaces
 
 get '/' do
-  "Hello World"
+  # "Hello World"
+  # erb(:cats)
+  # erb :"launchers/cats"
+  erb :cats
 end
 
 get "/cards" do
   @cards = ["Dark-Magician", "Exodia", "Trap-Hole", "Blue-Eyes-White-Dragon-Holofoil"]
-
+  # message = "hi everyone!"
   erb :cards
 end
 
-get "/cards-display/:favorite_card" do 
-
+get "/cards/:favorite_card" do
+  # binding.pry
   @card = params["favorite_card"]
-
-  erb :display
+  # @card = "Dark-Magician"
+  erb :show
 end
-
-
-
-
